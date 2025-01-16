6ae9c6a25e169751c1652f5ac5e007316c9dd374
Riski Maulana Rahman <61986465+riskimaulanarahman@users.noreply.github.com>
2025-01-16 23:16:02 +0800

    fix meta data

--- resources/views/layouts/master-layouts.blade.php
+++ resources/views/layouts/master-layouts.blade.php
@@ -5,10 +5,10 @@
     <meta charset="utf-8" />
     <title> @yield('title') | {{ env('APP_NAME') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Career Itci Hutani Manunggal" name="description" />
     <meta content="Themesbrand" name="author" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ URL::asset('assets/images/ihm-tp.png')}}">
     @include('layouts.head-css')
 </head>
 
